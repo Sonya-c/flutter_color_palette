4178c3d644dd738aa02f195777e4f11dddaa1ba9
examen <examen@CampusUninorte.local>
2023-08-16 10:49:11 -0500

    complited

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -24,21 +24,43 @@ class ColorSelectionPage extends StatelessWidget {
             IconButton(onPressed: showDialog, icon: Icon(Icons.info_outline))
           ],
         ),
-        // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
-        )));
+        // https://colorhunt.co
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ColorPalette(
+                  pallete: const ["79155B", "C23373", "F6635C", "FFBA86"],
+                  callback: showColor),
+              ColorPalette(
+                  pallete: const ["0C356A", "279EFF", "40F8FF", "D5FFD0"],
+                  callback: showColor),
+              ColorPalette(
+                  pallete: const ["EBE76C", "F0B86E", "ED7B7B", "836096"],
+                  callback: showColor),
+              ColorPalette(
+                  pallete: const ["E19898", "A2678A", "4D3C77", "3F1D38"],
+                  callback: showColor),
+              ColorPalette(
+                  pallete: const ["313866", "504099", "974EC3", "FE7BE5"],
+                  callback: showColor),
+              ColorPalette(
+                  pallete: const ["614BC3", "33BBC5", "85E6C5", "C8FFE0"],
+                  callback: showColor),
+              ColorPalette(
+                  pallete: const ["FFF3DA", "DFCCFB", "D0BFFF", "BEADFA"],
+                  callback: showColor)
+            ],
+          ),
+        ));
   }
 
-  // está es la función que será llamada con cada click a un ColorPalette
   void showColor(String value) {
     Clipboard.setData(ClipboardData(text: value));
     Get.snackbar(
       'Color palette',
       'Valor copiado',
       backgroundColor: ColorUtils.FromHex(value),
-      snackPosition: SnackPosition.BOTTOM,
+      snackPosition: SnackPosition.TOP,
     );
   }
 }
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -4,16 +4,16 @@ import '../../utils/color_utils.dart';
 class ColorPalette extends StatelessWidget {
   const ColorPalette({
     Key? key,
-    // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
+    required this.pallete,
+    required this.callback,
   }) : super(key: key);
 
   // atributos de la clase, por ejemplo
-  //final String baseColor;
+  final List<String> pallete;
 
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback;
 
   // el callback se puede llamar así en el GestureDetector
   //  onTap: () => callback(baseColor.toString())
@@ -21,11 +21,23 @@ class ColorPalette extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // padding para crear espacio alrededor del widget
+    List<Widget> colors = [];
+    for (int i = 0; i < pallete.length; i++) {
+      colors.add(Expanded(
+          flex: pallete.length - i,
+          child: GestureDetector(
+            onTap: () => callback(pallete[i]),
+            child: Container(
+              height: 100,
+              color: ColorUtils.FromHex(pallete[i]),
+            ),
+          )));
+    }
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
-    );
+        padding: const EdgeInsets.all(8.0),
+        // vamos a mostrar los elementos en un row
+        child: Row(
+          children: colors,
+        ));
   }
 }
